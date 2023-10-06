368b9ac17d3af0fe80aa7811c38cea5841313007
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2023-10-06 15:37:20 -0300

    Adjusts NavMenu

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -5,53 +5,56 @@ import styles from './styles.module.css'
 import { useState } from 'react'
 import Link from 'next/link'
 
-import { CloseOutlined, HomeOutlined, WorkOutlineOutlined, CodeOutlined, LocalPhone } from '@mui/icons-material'
+import { CloseOutlined, MenuOutlined, HomeOutlined, WorkOutlineOutlined, CodeOutlined, LocalPhone } from '@mui/icons-material'
 
 const NavMenu = () => {
-    const [activeMenu, setActiveMenu] = useState<boolean>(true);
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
-    const handleCloseMenu = (): void => {
-        setActiveMenu(false);
-    }
-
-    const handleOpenMenu = (): void => {
-        setActiveMenu(true);
+    const handleToggleMenu = (): void => {
+        setMenuOpen(!menuOpen);
     }
 
     return (
-        <div className={activeMenu ? styles.nav_container : styles.close_menu}>
-            <button onClick={handleCloseMenu}>
-                <CloseOutlined />
-            </button>
-            <nav className={styles.nav}>
-                <ul className={styles.menu}>
-                    <li>
-                        <Link href='#'>
-                            <HomeOutlined />
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href='#'>
-                            <CodeOutlined />
-                            Projects
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href='#'>
-                            <WorkOutlineOutlined />
-                            About
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href='#'>
-                            <LocalPhone />
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <>
+            <div className={`${styles.open_menu} ${menuOpen ? styles.close_menu : ''}`}>
+                <button onClick={handleToggleMenu}>
+                    <MenuOutlined />
+                </button>
+            </div>
+            <div className={`${styles.nav_container} ${menuOpen ? '' : styles.close_menu}`}>
+                <button onClick={handleToggleMenu}>
+                    <CloseOutlined />
+                </button>
+                <nav className={styles.nav}>
+                    <ul className={styles.menu}>
+                        <li>
+                            <Link href='#'>
+                                <HomeOutlined />
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='#'>
+                                <CodeOutlined />
+                                Projects
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='#'>
+                                <WorkOutlineOutlined />
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='#'>
+                                <LocalPhone />
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </>
     )
 }
 
