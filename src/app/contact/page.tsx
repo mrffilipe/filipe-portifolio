f352877bb340057f13caae485e4070547ce7caee
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2023-10-20 18:04:55 -0300

    Refactoring contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,26 +1,31 @@
 import styles from './styles.module.css'
 
-import { AlternateEmailOutlined, CallOutlined } from '@mui/icons-material'
 import Image from 'next/image'
+import Link from 'next/link'
 
-import LinkedingSvg from '../../assets/svg/linkedin.svg'
+import LinkedinSvg from '@/assets/svg/linkedin.svg'
+import GithubSvg from '@/assets/svg/github.svg'
 
 const Contact = () => {
     return (
-        <section className={styles.contact}>
+        <section id='contact' className={styles.contact}>
             <div className={styles.description}>
-                <h1>Contact</h1>
-                <div>
-                    <Image src={LinkedingSvg} alt="Linkedin icons8" />
-                    <a href="https://www.linkedin.com/in/filipemrf/" target='_blank'>filipemrf</a>
-                </div>
-                <div>
-                    <AlternateEmailOutlined />
-                    <span>mrffilipe@outlook.com</span>
-                </div>
-                <div>
-                    <CallOutlined />
-                    <span>+55 (64) 9 9645-9874</span>
+                <h2>Contact</h2>
+                <p>
+                    Interested in hiring me for your project or just to say hi? You can fill in the contact from below or send me an email to <span>mrffilipe@outlook.com</span>
+                </p>
+            </div>
+            <div className={styles.social_channels}>
+                <p>
+                    Want to get connected? Follow me on the social channels below.
+                </p>
+                <div className={styles.links}>
+                    <Link href="https://www.linkedin.com/in/filipemrf/" target='_blank'>
+                        <Image src={LinkedinSvg} alt="Linkedin icons8" />
+                    </Link>
+                    <Link href="https://github.com/mrffilipe" target='_blank'>
+                        <Image src={GithubSvg} alt="Github icons8" />
+                    </Link>
                 </div>
             </div>
         </section>
