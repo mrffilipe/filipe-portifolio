4a4669c3acd18a5d37241d63ca65fc2a577aa174
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2023-10-21 11:42:54 -0300

    Adjust to responsive Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,22 @@ import { ArrowCircleRight } from '@mui/icons-material'
 
 import WhatIDo from '@/components/WhatIDo'
 
-import LinkedinSvg from '../assets/svg/linkedin.svg'
-import GithubSvg from '../assets/svg/github.svg'
+import LinkedinSvg from '@/assets/svg/linkedin.svg'
+import GithubSvg from '@/assets/svg/github.svg'
 import FeaturedProjects from '@/components/FeaturedProjects'
 
 export default function Home() {
   return (
     <>
       <section id='about_me' className={styles.about_me}>
-        <div className={styles.description}>
-          <h1>Filipe Ferracioli</h1>
-          <h3>Software Engineer</h3>
-          <p>
-            A backend developer who loves to create solid and effective systems to enhance online experiences, building the foundation of the internet. Check out my project <a>portfolio</a>.
-          </p>
+        <div className={styles.presentation}>
+          <div className={styles.description}>
+            <h1>Filipe Ferracioli</h1>
+            <h3>Software Engineer</h3>
+            <p>
+              A backend developer who loves to create solid and effective systems to enhance online experiences, building the foundation of the internet. Check out my project <a>portfolio</a>.
+            </p>
+          </div>
           <div className={styles.profile}></div>
         </div>
         <div className={styles.actions}>
